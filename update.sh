e7afdd8158558e21643f2581e99840dbca811144
TrudeEH <ehtrude@gmail.com>
2023-02-16 11:31:29 +0000

    Update update script

--- update.sh
+++ update.sh
@@ -16,6 +16,25 @@ echo
 echo "Pull changes..."
 git pull
 
+if [ $? != 0 ]
+then
+	echo
+	echo "Git conflict detected!"
+	echo "Resetting the Git repository will fix the issue."
+	echo "Wipe your settings?"
+	read -p "y/n" wipe
+	if [ $wipe == "y" ]
+	then
+		echo "Wipe confirmed."
+		git reset --hard
+		echo "done."
+	else
+		echo "Update cancelled."
+		echo "The script will exit in 10 seconds."
+		sleep 10
+	fi
+fi
+
 echo
 echo "Run installer"
 ./install.sh
